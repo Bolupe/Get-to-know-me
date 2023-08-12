efeeb56cee49f3cb13994c2412a465666319ae21
Bolupe Taiwo <bolupetaiwo15@gmail.com>
2023-08-12 18:28:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Get to know me
+With this short quiz, you get to know me.
---
+++ javascript/questions.js
@@ -0,0 +1,58 @@
+// Creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What country do I live in?",
+        answer: "Nigeria",
+        options: [
+            "United Kingdom",
+            "United States",
+            "Nigeria",
+            "Canada"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What university do I attend?",
+        answer: "University of Lagos",
+        options: [
+            "University of Oxford",
+            "Harvard University",
+            "University of Lagos",
+            "University of Toronto"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What course am I studying?",
+        answer: "Building",
+        options: [
+            "Petroleum Engineering",
+            "Architecture",
+            "Building",
+            "Law"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Why I started learning Front-end Development?",
+        answer: "A friend told me to",
+        options: [
+            "Side hustle",
+            "Wanted to learn something new",
+            "A friend told me to",
+            "To feel geeky"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How many years of experience do I have in this field?",
+        answer: "Close to 2 years",
+        options: [
+            "A year",
+            "Over a year",
+            "Close to 2 years",
+            "Two years"
+        ]
+    },
+];
